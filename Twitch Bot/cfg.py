8f5f546b09778a2f900e41c518470d82b96c30ce
CelesMeh <60585813+Celesmeh@users.noreply.github.com>
2020-08-27 15:24:29 -0400

    Create cfg.py

---
+++ Twitch Bot/cfg.py
@@ -0,0 +1,6 @@
+# cfg.py
+HOST = "irc.twitch.tv"              # the Twitch IRC server
+PORT = 6667                         # always use port 6667!
+NICK = "thedicegodssay"            # your Twitch username, lowercase
+PASS = "oauth:cvbgmw0haqxtltngwla9nm1gtr7gtg" # your Twitch OAuth token
+CHAN = "#Celesmeh"  
