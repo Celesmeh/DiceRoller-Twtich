b17ef82252e7486b715a065a68d654acc57fb7a1
CelesMeh <60585813+Celesmeh@users.noreply.github.com>
2020-08-28 15:50:07 -0400

    wew new

---
+++ Twitch Bot/twitch_tut.py
@@ -0,0 +1,43 @@
+from irc.bot import SingleServerIRCBot
+from requests import get
+
+NAME = "thedicegodssay"
+OWNER = "celesmeh"
+
+class Bot(SingleServerIRCBot):
+	def __init__(self):
+		self.HOST = "irc.chat.twitch.tv"
+		self.PORT = 6667
+		self.USERNAME = NAME.lower()
+		self.CLIENT_ID = "j1jnt9v9s4fmvl05v9gqamuf2si5p8"
+		self.TOKEN = "jzhbx6n3ube8fp33o1txvxdcjnkvdg"
+		self.CHANNEL = f"#{OWNER}"
+
+		url = f"https://api.twitch.tv/kraken/users?login={self.USERNAME}"
+		headers = {"Client-ID": self.CLIENT_ID, "Accept": "application/vnd.twitchtv.v5+json"}
+		resp = get(url, headers=headers).json()
+		self.channel_id = resp["users"][0]["_id"]
+
+		super().__init__([(self.HOST, self.PORT, f"oauth:{self.TOKEN}")], self.USERNAME, self.USERNAME)
+
+	def on_welcome(self, cxn, event):
+		for req in ("membership", "tags", "commands"):
+			cxn.cap("REQ", f":twitch.tv/{req}")
+
+		cxn.join(self.CHANNEL)
+		self.send_message("I'm online.")
+
+	def on_pubmsg(self, cxn, event):
+		tags = {kvpair["key"]: kvpair["value"] for kvpair in event.tags}
+		user = {"name": tags["display-name"], "id": tags["user-id"]}
+		message = event.arguments[0]
+
+		print(f"Message from {user['name']}: {message}")
+
+	def send_message(self, message):
+		self.connection.privmsg(self.CHANNEL, message)
+
+
+if __name__ == "__main__":
+	bot = Bot()
+	bot.start()
